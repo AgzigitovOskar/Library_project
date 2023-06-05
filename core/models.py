9cd6d254d96ed101da1a7581f2c5744284060b44
Oskar Agzigitov <110802094+AgzigitovOskar@users.noreply.github.com>
2023-06-06 01:34:18 +0300

    commit corrected models

--- core/models.py
+++ core/models.py
@@ -9,21 +9,6 @@ class BaseModel(models.Model):
         abstract = True
 
 
-class Reader(BaseModel):
-    name = models.CharField('Имя', max_length=50)
-    surname = models.CharField('Фамилия', max_length=50)
-    phone = models.CharField('№ телефона', max_length=20)
-    is_active = models.BooleanField('Активен', default=True)
-    active_books = models.ManyToManyField('Book', max_length=3, blank=True, verbose_name="Взятые книги")
-
-    def __str__(self):
-        return f"{self.name} {self.surname}"
-
-    class Meta:
-        verbose_name = "Читатель"
-        verbose_name_plural = "Читатели"
-
-
 class Book(BaseModel):
     title = models.CharField('Название', max_length=50)
     author = models.ForeignKey('Author', on_delete=models.CASCADE, related_name='books', verbose_name="Автор")
@@ -51,3 +36,23 @@ class Author(BaseModel):
         verbose_name = "Автор"
         verbose_name_plural = "Авторы"
         unique_together = ('name', 'surname')
+
+
+class Reader(BaseModel):
+    class Status(models.TextChoices):
+        is_active = 'активен'
+        not_active = 'не активен'
+
+    name = models.CharField('Имя', max_length=50)
+    surname = models.CharField('Фамилия', max_length=50)
+    # phone = PhoneNumberField(unique=True, null=False, blank=False)
+    phone = models.CharField('№ телефона', max_length=20)
+    is_active = models.CharField('Активен', max_length=10, choices=Status.choices, default=Status.is_active)
+    active_books = models.ManyToManyField(Book, max_length=3)
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
+
+    class Meta:
+        verbose_name = "Читатель"
+        verbose_name_plural = "Читатели"
