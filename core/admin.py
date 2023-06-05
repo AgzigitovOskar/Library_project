e8a55276f7a799c8c05b685296840b0ddb4ef28c
Oskar Agzigitov <110802094+AgzigitovOskar@users.noreply.github.com>
2023-06-06 01:13:38 +0300

    commit corrected models

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
+from django.contrib.admin import register
+
 from .models import Reader, Book, Author
 
-admin.site.register(Book)
+# admin.site.register(Book)
 admin.site.register(Reader)
 admin.site.register(Author)
+
+
+@register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'quantity')
+    search_fields = ('title', 'description')
--- core/models.py
+++ core/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 
 
-class Reader(models.Model):
+class BaseModel(models.Model):
+    created_at = models.DateTimeField('Создано', auto_now_add=True)
+    edited_at = models.DateTimeField('Обновлено', auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Reader(BaseModel):
     name = models.CharField('Имя', max_length=50)
     surname = models.CharField('Фамилия', max_length=50)
     phone = models.CharField('№ телефона', max_length=20)
     is_active = models.BooleanField('Активен', default=True)
-    active_books = models.ManyToManyField('Book', blank=True, verbose_name="Взятые книги")
-    created_at = models.DateTimeField('Создано', auto_now_add=True)
-    edited_at = models.DateTimeField('Обновлено', auto_now=True)
+    active_books = models.ManyToManyField('Book', max_length=3, blank=True, verbose_name="Взятые книги")
 
     def __str__(self):
         return f"{self.name} {self.surname}"
@@ -18,14 +24,12 @@ class Reader(models.Model):
         verbose_name_plural = "Читатели"
 
 
-class Book(models.Model):
+class Book(BaseModel):
     title = models.CharField('Название', max_length=50)
     author = models.ForeignKey('Author', on_delete=models.CASCADE, related_name='books', verbose_name="Автор")
     description = models.TextField('Описание', null=True, blank=True)
     sheets = models.PositiveSmallIntegerField('Кол-во страниц')
     quantity = models.PositiveSmallIntegerField('Кол-во шт.', default=0)
-    created_at = models.DateTimeField('Создано', auto_now_add=True)
-    edited_at = models.DateTimeField('Обновлено', auto_now=True)
 
     def __str__(self):
         return f"{self.author.name[0].title()}.{self.author.surname} - {self.title}"
@@ -35,12 +39,10 @@ class Book(models.Model):
         verbose_name_plural = "Книги"
 
 
-class Author(models.Model):
+class Author(BaseModel):
     name = models.CharField('Имя', max_length=50)
     surname = models.CharField('Фамилия', max_length=50)
-    photo = models.ImageField(null=True, blank=True, upload_to='author_fotos/', verbose_name="Фотография")
-    created_at = models.DateTimeField('Создано', auto_now_add=True)
-    edited_at = models.DateTimeField('Обновлено', auto_now=True)
+    photo = models.ImageField('Фотография', upload_to='author_fotos/')
 
     def __str__(self):
         return f"{self.name} {self.surname}"
@@ -48,3 +50,4 @@ class Author(models.Model):
     class Meta:
         verbose_name = "Автор"
         verbose_name_plural = "Авторы"
+        unique_together = ('name', 'surname')
